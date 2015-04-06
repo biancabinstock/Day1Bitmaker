d22ffe5a993cb2ba0a7b79f94bb39170e7845ca2
Bianca Binstock <bianca.pr@gmail.com>
2015-04-06 16:17:49 -0400

    created a practice file called colour

---
+++ colour.h
@@ -0,0 +1,13 @@
+//
+//  colour.h
+//  BitmakerLabsDay1
+//
+//  Created by Bianca Binstock on 2015-04-06.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface colour : NSObject
+
+@end
---
+++ colour.m
@@ -0,0 +1,13 @@
+//
+//  colour.m
+//  BitmakerLabsDay1
+//
+//  Created by Bianca Binstock on 2015-04-06.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import "colour.h"
+
+@implementation colour
+
+@end
