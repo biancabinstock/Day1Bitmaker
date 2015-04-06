e49ba60a3143c933e36f9222f1c030f0d0e50b44
Bianca Binstock <bianca.pr@gmail.com>
2015-04-06 15:51:08 -0400

    added social model

---
+++ Feature.h
@@ -0,0 +1,13 @@
+//
+//  Feature.h
+//  BitmakerLabsDay1
+//
+//  Created by Bianca Binstock on 2015-04-06.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Feature : NSObject
+
+@end
---
+++ Feature.m
@@ -0,0 +1,13 @@
+//
+//  Feature.m
+//  BitmakerLabsDay1
+//
+//  Created by Bianca Binstock on 2015-04-06.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import "Feature.h"
+
+@implementation Feature
+
+@end
